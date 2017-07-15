f65f8a0c524f0bca35d87a3ca5f6e9f4772d1ae4
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2017-07-15 20:08:04 -0300

    correcoes de prints

--- cliente.py
+++ cliente.py
@@ -1,62 +1,58 @@
 import sys  ## import modulo
 import os ## import modulo do SO para trabalhar com funcionalidades que dependem do SO
 import socket
+import time
 
 # tornar o programa executavel
 #!/usr/bin/python
-
 #acentuacoes
 # -*- coding: utf-8 -*-
 
 # Funcao que mostra como deve ser a entrada, caso ocorra algum erro
-def informacoesEntrada():
-	print("Argumentos:")
+def argumentosEntrada():
+	print("Verifique os parametros de execucao do programa.\nArgumentos:")
 	print("python "+__file__+" <IPServer> <porta> <caminho_do_arquivo_coordenadas>")
 	sys.exit()
 
 def enviarDados(IPServer, porta, caminhoArquivo):
-	mensagem = socket_tcp_cliente.recv(4096)
+	mensagem = socket_tcp_cliente.recv(512)
 	if(mensagem == "PRONTO PARA RECEBER DADOS"):
 		try:
-			print "Enviando arquivo para " + IPServer + ":" + str(porta)
+			print (">>>Enviando arquivo " + caminhoArquivo + " para " + IPServer + ":" + str(porta) + "...")
 			arquivo = open(caminhoArquivo, 'r')
 			dados = arquivo.read();
 			socket_tcp_cliente.send(dados)
 			while dados != "":
 				dados = arquivo.read();
 				socket_tcp_cliente.send(dados)
-			# pra "avisar ao servidor que terminou o arquivo"
-			socket_tcp_cliente.send("EOF")
-			print "fim arquivo"
+			socket_tcp_cliente.send("EOF") # pra "avisar ao servidor que terminou o arquivo"
 			arquivo.close()
-
-			msg_concluido = socket_tcp_cliente.recv(4096)
-			if(msg_concluido == "DOWNLOAD CONCLUIDO"):
-				print "cliente baixou arquivo"
-				msg_processamento = socket_tcp_cliente.recv(4096)
-				if(msg_processamento == "PROCESSAMENTO CONCLUIDO"):
+			msg_concluido = socket_tcp_cliente.recv(512)
+			if(msg_concluido == "DOWNLOAD CONCLUIDO"): # servidor baixou o arquivo
+				msg_processamento = socket_tcp_cliente.recv(512)
+				if(msg_processamento == "PROCESSAMENTO CONCLUIDO"): #servidor conclui o processamento dos dados, preparar para receber a saida
 					socket_tcp_cliente.send("PRONTO PARA RECEBER RESPOSTA")
-					print "cliente pronto para receber resposta"
 					receberDados(caminhoArquivo)
 				else:
-					print "NAO FOI POSSIVEL PROCESSAR DADOS"
+					print (">>>Nao foi possivel processar os dados")
+					sys.exit()
 			else:
-				print "Erro ao enviar o arquivo"
-				#socket_tcp_cliente.close()
+				print (">>>Servidor nao concluiu o dowload!")
+				sys.exit() #socket_tcp_cliente.close()
 		except Exception as msg:
-			print("Error message: "+str(msg))
+			print(">>>Error message: "+str(msg))
 			return False
 		return True
 	elif(mensagem == "ERRO"):
-		print "servidor nao permitiu conexao por algum motivo"
+		print (">>>Servidor nao esta pronto para receber dados!")
 		return False
 	else:
-		print "Algum erro" + mensagem
+		print (">>>Algum erro" + mensagem)
 		return False
 
 def receberDados(caminhoArquivo):
 	caminhoArquivo = "SAIDA" + caminhoArquivo
-	print "pronto para receber"
+	print (">>>Recebendo resultado...")
 	try:
 		arq_temp = open(caminhoArquivo, 'w')
 		while True:
@@ -67,13 +63,12 @@ def receberDados(caminhoArquivo):
 			arq_temp.write(dados)
 		socket_tcp_cliente.send("DOWNLOAD CONCLUIDO")
 		socket_tcp_cliente.close()
+		time.sleep(5)
+		print (">>>Conexao encerrada!")
+	# caso ocorra alguma excecao
 	except Exception as msg:
-		print("Error message: "+str(msg))
+		print(">>>Error message: "+str(msg))
 		return False
-
-# Funcao para enviar o arquivo para o servidor
-#def enviarArquivo(IPServer, porta, caminhoArquivo):
-#    socket.sendfile(caminhoArquivo)
 
 # Le os argumentos da linha de comando, caso houver algum erro nos parametros, exibe mensagem de ajuda de como deve ser a entrada
 try:
@@ -81,24 +76,20 @@ try:
 	porta = int(sys.argv[2])
 	caminhoArquivo = sys.argv[3]
 except:
-	informacoesEntrada()
+	argumentosEntrada()
 
-socket_tcp_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#If the file exists
-#Start the connection with the server
+# verifica e o arquivo com as coordenadas existe, para iniciar a conexao
 if (os.path.exists(caminhoArquivo)):
 	try:
-		socket_tcp_cliente.connect((IPServer, int(porta)))
-		print "Conectado ao servidor " + IPServer + ":" + str(porta)
+		socket_tcp_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # cria socket do lado cliente utilizando IPV4 com protocolo TCP
+		socket_tcp_cliente.connect((IPServer, int(porta))) # conecta com o servidor
+		print (">>>Conectado ao servidor " + IPServer + ":" + str(porta))
 	except socket.error as msg:
-		print "Nao foi possivel conexao com o servidor " + IPServer + ":" + str(porta)
-		print msg
+		print (">>>Erro ao conectar com o servidor " + IPServer + ":" + str(porta))
+		print (">>>"+msg)
 		sys.exit()
-
 	socket_tcp_cliente.send("ENVIANDO DADOS")
 	enviarDados(IPServer,porta,caminhoArquivo)
-
 else:
-	print("File does not exists.")
+	print(">>>Arquivo nao encontrado!")
 	sys.exit()
