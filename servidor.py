12cb616e0c2e5645de51af846871e1030ed00e09
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2017-07-15 20:08:50 -0300

    correcoes de prints

--- servidor.py
+++ servidor.py
@@ -1,8 +1,8 @@
-import sys  ## import modulo
 import os ## import modulo do SO para trabalhar com funcionalidades que dependem do SO
 import socket
+import sys
 import thread
-import uuid
+import time
 from math import radians, cos, sin, asin, sqrt
 # tornar o programa executavel
 #!/usr/bin/python
@@ -12,8 +12,9 @@ from math import radians, cos, sin, asin, sqrt
 
 # Echo server program
 HOST = ''
-PORT = 8095              # Arbitrary non-privileged port
+PORT = int(sys.argv[1])
 
+## funcao de haversine, que calcula a distancia entre dois pontos
 def haversine(lat1,long1,lat2,long2):
     RAIO_PLANETA_TERRA = 6371  # in km
     # convert all latitudes/longitudes from decimal degrees to radians
@@ -21,27 +22,21 @@ def haversine(lat1,long1,lat2,long2):
     # calculate haversine
     latitude = lat2 - lat1
     longitude = long2 - long1
-
     d = sin(latitude * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(longitude * 0.5) ** 2
     h = 2 * RAIO_PLANETA_TERRA * asin(sqrt(d))
     return h
 
 # funcao que faz o processamento dos dados enviados pelo cliente
 def processarArquivo(conexao, arquivo_temporario):
-    print "processando dados"
+    print (">>>Processando dados...")
     try:
         arq_temp = open(arquivo_temporario, 'r')
-        print "nome arquivo temporario --->", arquivo_temporario
         saida_temp = "SAIDA_" + arquivo_temporario
-        print saida_temp
         arquivo_saida = open (saida_temp, 'w')
         ### processa linha por linha e escreve no arquivo de saida
-
         for linha in arq_temp:
-            ### remover /n do final
-            linhaTemp = linha[0:-1]
-            #separar valores
-            separarVirgula = linhaTemp.split(',')
+            linhaTemp = linha[0:-1] ### remover /n do final
+            separarVirgula = linhaTemp.split(',') #separar valores
             #tirar todos os espacos
             lat1 = separarVirgula[0].split()
             long1 = separarVirgula[1].split()
@@ -52,15 +47,14 @@ def processarArquivo(conexao, arquivo_temporario):
             long1 = float(long1[0])
             lat2 = float(lat2[0])
             long2 = float(long2[0])
-            ### tratar dados
-            distancia = haversine(lat1,long1,lat2,long2)
-            ### escrever as coordenadas e a distancia no arquivo
-            saida = str(lat1) + "," + str(long1) + "," + str(lat2) + "," + str(long2) + "," + str(distancia) + "\n"
+            distancia = haversine(lat1,long1,lat2,long2) ### tratar dados
+            saida = str(lat1) + ", " + str(long1) + ", " + str(lat2) + ", " + str(long2) + ", " + str(distancia) + "\n" ### escrever as coordenadas e a distancia no arquivo
             arquivo_saida.write(saida)
         arq_temp.close()
+        os.remove(arquivo_temporario)
         arquivo_saida.close()
     except:
-        print "Erro ao abrir arquivo"
+        print (">>>Erro ao abrir arquivo")
         sys.exit()
 
 # funcao que faz a comunicacao com o cliente fazendo a leitura do arquivo ( que o cliente esta enviando ao server)
@@ -75,97 +69,85 @@ def receberDados(conexao, caminhoArquivo):
                 break
             arq_temp.write(dados)
         conexao.send("DOWNLOAD CONCLUIDO")
-
-        ## processar dados do "fileTemp.csv aqui"
         processarArquivo(conexao, arquivo_temporario)
-        ## enviar para o cliente o resultado
         conexao.send("PROCESSAMENTO CONCLUIDO")
     except Exception as msg:
-        conexao.send("ERROR")
-        #File Error.
-        print("Error message: "+str(msg))
+        conexao.send("ERRO") #File Error.
+        print(">>>Error message: "+str(msg))
         return
 
-# funcao inicia uma nova thread para conexao
-def infoConexao(conexao, endereco_cliente):
-    mensagem = conexao.recv(1024)
+# funcao com a conexao com o cliente, depois de iniciada a thread
+def CONEXAO(conexao, endereco_cliente):
+    mensagem = conexao.recv(512)
     if (mensagem=="ENVIANDO DADOS"):
-        print "Conexao estabelecida com " + str(endereco_cliente)
         conexao.send("PRONTO PARA RECEBER DADOS")
         caminhoArquivo = str(endereco_cliente[1])
         receberDados(conexao, caminhoArquivo)
-
-        msg_retorno = conexao.recv(1024)
+        msg_retorno = conexao.recv(512)
         if(msg_retorno == "PRONTO PARA RECEBER RESPOSTA"):
-            enviarDados(conexao, caminhoArquivo)
+            enviarDados(conexao, caminhoArquivo, endereco_cliente)
     else:
         conexao.close()
     thread.exit()
 
-def enviarDados(conexao, caminhoArquivo):
-    print "Enviando dados cliente"
+def enviarDados(conexao, caminhoArquivo, endereco_cliente):
+    print (">>>Enviando dados para cliente...")
     caminhoArquivo = "SAIDA_" + caminhoArquivo + "fileTemp.csv"
-    print "aqui tem corrage ----> " + caminhoArquivo
     try:
         arquivo = open(caminhoArquivo, 'r')
         dados = arquivo.read()
         conexao.send(dados)
         while dados != "":
-            print '[' + dados + ']'
+            #print ('[' + dados + ']')
             dados = arquivo.read();
             conexao.send(dados)
         conexao.send("EOF")
         arquivo.close()
-
-        msg_download_concluido = conexao.recv(4096)
+        os.remove(caminhoArquivo) #para remover o arquivo temporario do diretorio
+        msg_download_concluido = conexao.recv(512)
         if(msg_download_concluido == "DOWNLOAD CONCLUIDO"):
-            print "Encerrar conexao"
+            print (">>>Encerrando conexao...")
             conexao.close()
+            time.sleep(5)
+            print (">>>Conexao com " + str(endereco_cliente) + " encerrada!")
         else:
-            print "DOWNLOAD NAO CONCLUIDO"
+            print (">>>DOWNLOAD NAO CONCLUIDO")
             #fechar conexao igual
-
         conexao.close()
     except:
-        print "ALGUM ERRO"
+        print (">>>Erro ao enviar resultado para o cliente")
         return False
     return True
 
 ### cria socket Ipv4
 socket_tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 #para zerar o TIME_WAIT do socket
 socket_tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-
 ### aciona porta para ouvir
-
 try:
     socket_tcp_server.bind((HOST, PORT))
-    print "Porta ativa - ouvindo"
+    print (">>>Porta " + str(PORT) + " ativa")
 except socket.error as msg:
-    print "Erro bind"
-    print msg
+    print (">>>Erro bind")
+    print (">>>" + msg)
 
 try:
     socket_tcp_server.listen(5)
-    print "Ouvindo..."
+    print (">>>Ouvindo...")
 
 except socket.error as msg:
-    print msg
-    print "Erro listen"
-
+    print (">>>"+msg)
+    print (">>>Erro listen")
 
 try:
     while 1:
         conexao, endereco_cliente = socket_tcp_server.accept()
-        print 'Conectado por ', endereco_cliente
-        ##A thread will be create for each connection
-        #so, more than one client can be attended
-        thread.start_new_thread(infoConexao, tuple([conexao, endereco_cliente]))
+        print (">>>Conexao TCP ativa com " + str(endereco_cliente))
+
+        #cria thread para cada cliente q solicitar conexao
+        thread.start_new_thread(CONEXAO , tuple([conexao, endereco_cliente]))
 
 except KeyboardInterrupt:
-    print("")
-    print("Stop listening and TCP closed.")
+    print(">>>Parando de ouvir e encerrando conexao TCP")
     socket_tcp_server.close()
     sys.exit()
